926eb70a4d0eaf611a222f13885f567c1e8b5e97
Ducky-016 <deuce010606@example.com>
2026-06-30 19:30:15 +0800

    yabadabadoo

---
+++ src/js/HomePage.js
@@ -0,0 +1,3 @@
+loadComponent('Main-placeholder', '../components/homepage/MainDisplay.html');
+loadComponent('DrinkCategories-placeholder', '../components/homepage/DrinkCategories.html');
+loadComponent('Reviews-placeholder', '../components/homepage/CustomerReviews.html');
