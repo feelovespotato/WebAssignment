260ae18a5324522f5f3f5497bc93fcc04281b183
Ryan <deuce010606@gmail.com>
2026-06-30 20:30:13 +0800

    messing with home page colors

---
+++ src/js/theme.js
@@ -0,0 +1,7 @@
+const themeButton = document.getElementById('WebsiteTheme');
+
+themeButton.addEventListener('click', () => {
+  const html = document.documentElement;
+  const isDark = html.getAttribute('data-theme') === 'dark';
+  html.setAttribute('data-theme', isDark ? 'light' : 'dark');
+});
